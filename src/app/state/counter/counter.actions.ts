841b89889f65337de5b2a37c2363f3014040cffe
kewablay <kojokewajunior123@gmail.com>
2024-08-21 10:33:53 +0000

    added counter interval and reset functionality to reducer and actions

--- src/app/state/counter/counter.actions.ts
+++ src/app/state/counter/counter.actions.ts
@@ -1,16 +1,11 @@
-import { createAction } from "@ngrx/store";
+import { createAction, props } from '@ngrx/store';
 
+export const increment = createAction('[Counter Component] Increment');
 
+export const decrement = createAction('[Counter Component] Decrement');
 
-export const increment = createAction(
-    '[Counter Component] Increment'
-)
+export const reset = createAction('[Counter Component] Reset');
 
-export const decrement = createAction(
-    '[Counter Component] Decrement'
-
-)
-
-export const reset = createAction(
-    '[Counter Component] Reset'
-)
+export const setCountInterval = createAction(
+  '[Counter Component] setCountInterval', props<{interval: number}>()
+);
--- src/app/state/counter/counter.reducer.ts
+++ src/app/state/counter/counter.reducer.ts
@@ -1,17 +1,18 @@
 import { createReducer, on } from '@ngrx/store';
-import { decrement, increment, reset } from './counter.actions';
+import { decrement, increment, reset, setCountInterval } from './counter.actions';
 
 export interface CounterState {
   count: number;
   interval: number;
 }
 
-export const initialState = JSON.parse(localStorage.getItem('counter') ?? '{count : 0, interval: 1}');
+export const initialState = JSON.parse(localStorage.getItem('counter') ?? JSON.stringify({count : 0, interval: 1}));
 // export const initialState: CounterState = {count : 0, interval: 1};
 
 export const counterReducer = createReducer(
   initialState,
-  on(increment, (state: CounterState) => ({ ...state, count: state.count + 1 })),
-  on(decrement, (state:CounterState) => ({ ...state, count: state.count > 0 ? state.count - 1 : state.count })),
-  on(reset, (state:CounterState) => ({ ...state, count: 0 })),
+  on(increment, (state: CounterState) => ({ ...state, count: state.count + state.interval })),
+  on(decrement, (state:CounterState) => ({ ...state, count: state.count > 0 ? state.count - state.interval : state.count })),
+  on(setCountInterval, (state:CounterState, { interval }) => ({ ...state, interval })),
+  on(reset, (state:CounterState) => ({ ...state, count: 0, interval: 1 })),
 );
